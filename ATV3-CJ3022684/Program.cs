aba0e8f765534e8d173d8e465be306c22db4087d
CJ3022684 <CJ3022684@LAB01-PC12>
2024-03-06 09:09:21 -0300

    Adicionar arquivos de projeto.

---
+++ ATV3-CJ3022684/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ATV-CJ3022684
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite sua nota:");
+        int grade = Convert.ToInt32(Console.ReadLine());
+
+        if (grade >= 6)
+        {
+            Console.WriteLine("Aprovado");
+        {
+         else
+        }
+            Console.WriteLine('reprovado');
+        }
+    }
+}
